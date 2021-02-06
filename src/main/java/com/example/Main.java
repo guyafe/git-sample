24dc3e7e5ed36ed048e81812584e5e50e01b3ee9
Tal Yafe <talyafe85@gmail.com>
2021-02-06 11:32:23 +0200

    Change to Brackets

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class Main {
 
-  public static void main(String ... args){
+  public static void main(String[] args){
     new Child("Gad").speak();
     new Child("Dan").speak();
   }
